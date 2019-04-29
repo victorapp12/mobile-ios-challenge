77ad93d09b2c3c3e51cb91e6605065b716ab84f0
Victor Palhares <victor.perdomo12@gmail.com>
2019-04-28 21:15:44 -0300

    Message error enum

---
+++ Amaro WatchKit Extension/Configuration/MessageError.swift
@@ -0,0 +1,15 @@
+//
+//  MessageError.swift
+//  Amaro WatchKit Extension
+//
+//  Created by Victor Palhares on 28/04/2019.
+//  Copyright © 2019 Victor Palhares. All rights reserved.
+//
+
+import Foundation
+
+enum MessageError {
+    case NoInternetConnection
+    case NoResults
+    case SomethingWrong
+}
---
+++ Amaro WatchKit Extension/Scenes/Catalog/CustomViews/ProductRow.swift
@@ -0,0 +1,9 @@
+//
+//  ProductRow.swift
+//  Amaro WatchKit Extension
+//
+//  Created by Victor Palhares on 28/04/2019.
+//  Copyright © 2019 Victor Palhares. All rights reserved.
+//
+
+import Foundation
